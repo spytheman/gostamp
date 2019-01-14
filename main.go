a5a2ded2e4ac50e46051d9b1b0a808f36d847601
Delyan Angelov <delian66@gmail.com>
2019-01-14 17:50:30 +0200

    First working draft. Implement support for the options -color , -absolute , -start , -end .

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"github.com/spytheman/gostamp/terminal"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 type programSettings struct {
 	showVersion bool
+	useColor    bool
+	useAbsolute bool
+	showStart   bool
+	showEnd     bool
 }
 
 var (
@@ -26,10 +35,21 @@ func init() {
 		flag.PrintDefaults()
 	}
 	flag.BoolVar(&settings.showVersion, "version", false, "show the tool version")
+	flag.BoolVar(&settings.useColor, "color", true, "colorize the output")
+	flag.BoolVar(&settings.useAbsolute, "absolute", false, "use absolute timestamps")
+	flag.BoolVar(&settings.showStart, "start", true, "timestamp the start of the execution")
+	flag.BoolVar(&settings.showEnd, "end", true, "timestamp the end of the execution")
 	flag.Parse()
+	//fmt.Println(settings)
 	if settings.showVersion {
 		fmt.Println(version)
 		os.Exit(0)
+	}
+	if !settings.useColor {
+		terminal.TurnOffColor()
+	}
+	if settings.useAbsolute {
+		terminal.TurnOnAbsoluteTimestamps()
 	}
 }
 
@@ -39,10 +59,67 @@ func main() {
 		os.Exit(1)
 	}
 	cmdline = strings.Join(flag.Args(), " ")
-	fmt.Printf("Running command: '%s' ...\n", cmdline)
+	//fmt.Printf("Running command: '%s' ...\n", cmdline)
+
 	command := exec.Command(flag.Args()[0], flag.Args()[1:]...)
-	startError := command.Start()
+
+	commandIn, commandInErr := command.StdinPipe()
+	if commandInErr != nil {
+		log.Panic(commandInErr)
+	}
+	commandOut, commandOutErr := command.StdoutPipe()
+	if commandOutErr != nil {
+		log.Panic(commandOutErr)
+	}
+	commandErr, commandErrErr := command.StderrPipe()
+	if commandErrErr != nil {
+		log.Panic(commandErrErr)
+	}
+
+	scannerOut := bufio.NewScanner(commandOut)
+	scannerErr := bufio.NewScanner(commandErr)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		_, err := io.Copy(commandIn, os.Stdin);
+		if err != nil {
+			log.Fatal(err)
+		}
+		commandIn.Close()
+	}()
+
+	go func() {
+		defer wg.Done()
+		for scannerErr.Scan() {
+			terminal.Err(scannerErr.Text())
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for scannerOut.Scan() {
+			terminal.Out(scannerOut.Text())
+		}
+	}()
+
+	if settings.showStart {
+		terminal.Out("Start of '" + cmdline + "'")
+	}
+
+	startErr := command.Start()
+	if startErr != nil {
+		log.Panic(startErr)
+	}
+
 	waitError := command.Wait()
-	fmt.Println("Start error: ", startError)
-	fmt.Println("Wait error: ", waitError)
+	if waitError != nil {
+		terminal.Err( "-->finished with error: " + waitError.Error() )
+	}
+
+	if settings.showEnd{
+		terminal.Out("End of '" + cmdline + "'")
+	}
+
+	wg.Wait()
 }
---
+++ terminal/terminal.go
@@ -0,0 +1,84 @@
+package terminal
+
+import (
+	"fmt"
+	"time"
+)
+
+func terminalColor2terminalCode(r, g, b uint8) int {
+	rr := (int(r) * 5) / 0xFF
+	gg := (int(g) * 5) / 0xFF
+	bb := (int(b) * 5) / 0xFF
+	return 36*rr + 6*gg + bb + 16
+}
+
+// Foreground returns the closest matching terminal foreground color escape sequence
+func terminalColorCodeFG(r, g, b uint8) string {
+	return fmt.Sprintf("\033[38;5;%dm", terminalColor2terminalCode(r, g, b))
+}
+
+// Background returns the closest matching terminal background color escape sequence
+func terminalColorCodeBG(r, g, b uint8) string {
+	return fmt.Sprintf("\033[48;5%dm", terminalColor2terminalCode(r, g, b))
+}
+
+// This escape code resets the terminal foreground and background colors
+const terminalColorCodeReset = "\033[0;00m"
+
+var tColorStdOut = ""
+var tColorStdErr = ""
+var tColorLineEnd = ""
+
+func TurnOffColor() {
+	tColorStdErr = "stderr: "
+	tColorStdOut = "stdout: "
+	tColorLineEnd = ""
+}
+
+func TurnOnColor() {
+	tColorStdOut = terminalColorCodeReset + terminalColorCodeFG(128, 255, 128) + terminalColorCodeBG(0, 0, 0)
+	tColorStdErr = terminalColorCodeReset + terminalColorCodeFG(255, 0, 0) + terminalColorCodeBG(0, 0, 0)
+	tColorLineEnd = terminalColorCodeReset
+}
+
+var absoluteTimestamps = false
+func TurnOnAbsoluteTimestamps() {
+	absoluteTimestamps = true
+}
+
+var t time.Time
+func init() {
+	TurnOnColor()
+}
+
+func Out(s string) {
+	lineOut(tColorStdOut, s)
+}
+
+func Err(s string) {
+	lineOut(tColorStdErr, s)
+}
+
+func lineOut(tCode string, s string) {
+	if absoluteTimestamps {
+		now := time.Now()
+		fmt.Printf("%s[%04d-%02d-%02d %02d:%02d:%02d.%06d]%s %s\n",
+			tCode,
+			now.Year(), now.Month(), now.Day(),
+			now.Hour(), now.Minute(), now.Second(),
+			now.Nanosecond()/1000,
+			tColorLineEnd,
+			s)
+	}else{
+		if t.IsZero() {
+			t = time.Now()
+		}
+		fmt.Printf("%s[%12s]%s %s\n",
+			tCode,
+			time.Since(t).String(),
+			tColorLineEnd,
+			s)
+		t = time.Now()
+	}
+}
+
